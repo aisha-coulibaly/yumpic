8dbe6caa13813a0ac27bd459934a344ca3083bd2
aisha.coulibaly <creepcxxp78@gmail.com>
2020-07-31 08:11:19 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザーを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:dander] = 'ユーザーの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報を編集しました。'
+        redirect_to @user
+      else
+        flash.now[:danger]
+        render :edit
+      end
+    else
+      redirect_to root_url
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
+end
