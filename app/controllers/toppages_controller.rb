f7ad604899f73a866bcce14952974a317179c94f
aisha.coulibaly <creepcxxp78@gmail.com>
2020-08-06 07:50:45 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,8 @@
 class ToppagesController < ApplicationController
   def index
       @posts = Post.all
+    if logged_in?
+      @posts = current_user.feed_posts.order(id: :desc).page(params[:page])
+    end
   end
 end
