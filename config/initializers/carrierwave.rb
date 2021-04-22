ea0a64b141ec4057ed2ddd91a8dd09345f1dd117
aisha.coulibaly <creepcxxp78@gmail.com>
2021-04-22 06:05:04 +0000

    img save to s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,17 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: 'AKIASRJ4YSMWH3MTMA4N',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1'
-    }
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-    config.fog_directory  = 'krcn-awsbucket'
-    config.cache_storage = :fog
-  end
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        
+  config.fog_credentials = {
+    provider:              'AWS',                        
+    aws_access_key_id:     Rails.application.credentials.aws[:access_key_id], 
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],     
+    region:                'ap-northeast-1'                  
+
+  }
+  config.fog_directory  = 'krcn-awsbucket'                
+  config.fog_public     = false                                                 
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
 end
